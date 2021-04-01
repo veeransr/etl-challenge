213669d190248425a1d03767154da223b9346471
Ryan Whitten <ryan.whitten@bestegg.com>
2021-04-01 16:53:39 -0400

    Update data file extension

--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ class EtlScript:
     def __init__(self):
         self.database_conn = Database("acme")
         self.header_file = "headers.txt"
-        self.data_file = "data.tsv"
+        self.data_file = "data.csv"
         self.out_file = "output.csv"
 
     def load_file_to_database(self, file_path: str):
