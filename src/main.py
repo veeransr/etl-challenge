a8a35ac5537ab4d38449eea55b830ec1c12ce502
Sudhakar Veeranki <veeransr@gmail.com>
2021-04-26 11:40:26 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -16,22 +16,22 @@ class EtlScript:
     def load_file_to_database(self, file_path: str):
         self.database_conn.load_file(file_path)
 
-    def run(self):        
+    def run(self):
         try:
-          with open (self.header_file, 'r') as head:
-            headers = [i for i in  head.read().split('\n') if i != '']
-            header_row = '|'.join(headers) 
-            with open(self.out_file, 'a') as new:
-                new.write(str(header_row))
-                new.write('\n')
-                with open (self.data_file, 'r') as csv_file:
-                    for line in csv_file.read():
-                      new.write(line)
+            with open (self.header_file, 'r') as head:
+                  headers = [ column_label for column_label in  head.read().split('\n') if column_label != '']
+                  header_row = '|'.join(headers) 
+                  with open(self.out_file, 'a') as new:
+                        new.write(str(header_row))
+                        new.write('\n')
+                        with open (self.data_file, 'r') as csv_file:
+                              for line in csv_file.read():
+                                new.write(line)
         except Exception as err:
-          logger.error('Could not process the ouput file due to {}.\
-                  Appending headers/loading to database was unsuccessful'.format(err))
+                logger.error('Could not process the ouput file due to {}.\
+                    Appending headers was unsuccessful'.format(err))
         else:
-          self.load_file_to_database(self.out_file)
+              self.load_file_to_database(self.out_file)
 
 if __name__ == "__main__":
     EtlScript().run()
